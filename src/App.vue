0ca3c91e6577b6f4290c4cc92768205876bd35d8
Samuel Filipe Faria <samuel.filipe@prevision.com.br>
2022-12-28 23:54:10 -0300

    Creating the form and the array of tasks

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
 <template lang="pug">
+div(class="container grid-xs py-2")
+  h1(class="text-center") Todo list vue
+  form(@submit.prevent="addTask(currentTask)")
+    div(class="input-group")
+      input(type="text" v-model="currentTask.description" class="form-input" placeholder="Item a fazer...")
+      button(class="btn btn-primary input-group-btn")
+        i(class="icon icon-plus")
 </template>
 
 <script>
@@ -6,6 +13,21 @@
 export default {
   name: 'App',
   components: {
+  },
+  data() {
+    return {
+      todoTasks: [],
+      currentTask: {
+        description: ""
+      },
+    };
+  },
+  methods: {
+    addTask(currentTask) {
+      currentTask.id = Date.now() + Math.floor(Math.random() * 100)
+      this.todoTasks.push(this.currentTask);
+      this.currentTask = { description: "" };
+    }
   }
 }
 </script>
